e465d6fcb22a9cf7a4623c1b881976d061efb84e
Michał <durajczykmichal@gmail.com>
2022-12-19 09:26:17 +0100

    commit

--- firstDjango/posts/models.py
+++ firstDjango/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    # Pole tekstowe o określonej długosci
+    content = models.TextField()
+    published = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
