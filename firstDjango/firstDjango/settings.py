2287b7574660650a8d6cb3d8b43cefb34cbdab68
Michał <durajczykmichal@gmail.com>
2023-02-20 10:29:44 +0100

    commit

--- firstDjango/firstDjango/settings.py
+++ firstDjango/firstDjango/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SHELL_PLUS_PRINT_SQL = True
