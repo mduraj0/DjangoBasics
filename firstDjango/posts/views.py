81b59c5404974c619a0263ee4310a5d55a24d30f
Michał <durajczykmichal@gmail.com>
2023-02-20 16:20:29 +0100

    commit

--- firstDjango/posts/views.py
+++ firstDjango/posts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from posts.models import Post
 
-# Create your views here.
+
+def posts_list(request):
+    htlm = """<ul>"""
+    for post in Post.objects.all():
+        htlm += f'<li>{post}</li>'
+
+    htlm += "</ul>"
+    return HttpResponse(htlm)
+
+
+def first_post(request):
+    post = Post.objects.first()
+    html = '<h2>' + post.title + '</h2>'
+    html += f'''<div>
+        <small>Utworzono {post.created}, zmodyfikowano {post.modified} </small>
+    </div>
+    <div>
+    <p> {post.content} </p>
+    </div>'''
+
+    return HttpResponse(html)
