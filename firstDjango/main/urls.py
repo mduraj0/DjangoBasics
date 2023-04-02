8663907d4b074c81254ce5ddf7a4bab720ac9d5b
Michał <durajczykmichal@gmail.com>
2023-04-02 14:50:31 +0200

    creating some_test.html, adding templates,

--- firstDjango/main/urls.py
+++ firstDjango/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from main.views import hello
+from main.views import hello, some_test
 
 
 urlpatterns = [
-    path('', hello)
+    path('', hello),
+    path('testy', some_test)
 ]
--- firstDjango/main/views.py
+++ firstDjango/main/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 
 def hello(request):
     return HttpResponse('Hello world')
+
+
+def some_test(request):
+    age = 26
+    first_name = 'Michael'
+    parents = ['Mom', 'Dad']
+    programming_l = {
+        'python' : 'advanced',
+        'sql' : 'beginner',
+        'ml' : 'beginner'
+    }
+    books = set(['Clean code', 'biblia_mysql'])
+    return render(request, 'main/some_test.html', context={
+        'age' : age,
+        'parents' : parents,
+        'programming_language' : programming_l,
+        'books' : books
+    })
