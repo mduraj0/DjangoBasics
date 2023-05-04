76ccb154b5881e9e0fde5e4d4fc464e9541b5b07
Michał <durajczykmichal@gmail.com>
2023-05-04 13:43:41 +0200

    adding forms, new file in templates in posts, adding views for add post and adding routing

--- firstDjango/posts/urls.py
+++ firstDjango/posts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import posts_list, post_details
+from .views import posts_list, post_details, add_post_form
 
 
 app_name = 'posts'
 
 urlpatterns = [
+    path('add/', add_post_form, name='add'),
     path('<int:post_id>', post_details, name='details'),
     path('', posts_list, name='list'),
-
 ]
--- firstDjango/posts/views.py
+++ firstDjango/posts/views.py
@@ -13,5 +13,17 @@ def post_details(request, post_id):
 def posts_list(request):
     posts = Post.objects.filter(published=True)
     context = {'posts_list': posts}
-
     return render(request, 'posts/list.html', context)
+
+
+def add_post_form(request):
+    if request.method == 'POST' and request.user.is_authenticated:
+        data = {
+            'title': request.POST['title'],
+            'content': request.POST['content'],
+            'sponsored': request.POST.get('sponsored', False),
+            'published': request.POST.get('published', False),
+            'author': request.user
+        }
+        post = Post.objects.create(**data)
+    return render(request, 'posts/add.html', {})
